913d74cce2a2fe63065696dd8a506e2194513099
Mohit-S-Rane <67183798+Mohit-S-Rane@users.noreply.github.com>
2022-03-08 19:20:57 +0530

    Update DB URL

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export  const environment :Environment =  {
     production : false,
     db_url: 'mongodb+srv://admin:admin@cluster0.ybvxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
-
+ 
 
 //mongodb://shagun:GARGSHAGUN15432@ds139939.mlab.com:39939/resume-dev
 //
