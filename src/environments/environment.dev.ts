66449f6e0831b8eab673cbbaf538f3e4247c4ed2
Mohit-S-Rane <67183798+Mohit-S-Rane@users.noreply.github.com>
2022-04-26 19:17:02 +0530

    Back-End project setup done

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,7 @@ import {Environment} from "./environment.interface";
 export  const environment :Environment =  {
     production : false,
     db_url: 'mongodb+srv://admin:admin@cluster0.ybvxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 };
  
 
