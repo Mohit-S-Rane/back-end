e2a575af7b3b8585d521b6709a8cccd9d819ed40
Mohit-S-Rane <67183798+Mohit-S-Rane@users.noreply.github.com>
2022-05-04 21:54:11 +0530

    Change to database for ResumeBuilder DB

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ import {Environment} from "./environment.interface";
 
 export  const environment :Environment =  {
     production : false,
-    db_url: 'mongodb+srv://admin:admin@cluster0.ybvxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    db_url: 'mongodb+srv://admin:admin@cluster101.nsbgg.mongodb.net/resumeBuilder?retryWrites=true&w=majority'
 
 };
  
